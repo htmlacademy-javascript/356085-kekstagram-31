8a8feba032db6afb200da7c6f8d55ec3f1acb207
Keks <keks@htmlacademy.ru>
2024-04-18 18:35:12 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,9 @@
+// import { photoCard, commentsCard } from './data';
+// import './drawing-thumbnails.js';
+// import { galleryPhotos } from './drawing-thumbnails.js';
+import './modal-picture.js';
+import './upload-form.js';
+import './pristine-validator.js';
+// import { openModalForm, closeModalForm } from './upload-form.js';
+
+
